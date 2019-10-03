c249edb67f0ddf37307ab606d7c99a93147e21d9
thejakemister <51132156+thejakemister@users.noreply.github.com>
2019-10-03 13:46:39 -0600

    upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# DA_RPG
-text based adventure, hints of horror and mystery. placed in a forest.
+# Escape the Forest
+
+escape the forest is text based advenute game that leads the player through
+a Forest
+
+## Installation
+
+Python 3.7 is needed to play this game. [Python 3.7](https://www.python.org/downloads/) to install Python.
+
+
+## Run the Program
+
+```python
+python da.py
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+LICENSE
---
+++ da.py
@@ -0,0 +1,80 @@
+# Course: CS 30
+# Period: 4
+# Date created: 23/10/01
+# Date last modified: 23/10/01
+# Name: Jacob Leippi
+# Description: my text based adventure game
+
+"""
+-------------------------------------------------------------------------------
+Base needs:
+
+inventory
+- canteen
+available weapons
+- spear
+- shotgun
+- swiss knife
+- availabe items
+enemies
+- wolf
+- hallucinations
+-------------------------------------------------------------------------------
+    mechanics
+                        -day and night system:
+                        ex. create a timer
+                        x = x + 1 idk
+                        -health system:
+                        ex. you encounter a wolf type defend to defend
+                        hp = health - (variable)
+                        -damage system:
+                        ex. you encounter a wolf type attack to attack
+                        enemy_hp = wolf_hp - (variable)
+                        -rng based pathway system
+                        ex. if you go a certian pathway and go back, that
+                        pathway may change
+-------------------------------------------------------------------------------
+Story:
+
+Game starts
+
+you awake in a forest (paint detail picture)
+-------------------------------------------------------------------------------
+ START():
+    you have three starting options:
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+go to waterfall South
+    options:
+        - swim:
+
+        - collect water:
+
+        - go back(North):
+            - go back to START.
+-------------------------------------------------------------------------------
+go to campsite East
+        - fire place
+
+        - tent
+
+        - backpacks
+
+        - go back(West):
+            - go back to START.
+-------------------------------------------------------------------------------
+go into the trees North
+        - North
+
+        - East
+
+        - West
+
+        - go back(South):
+            - go back to START.
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+END GAME:
+    you arrive at old oak tree behind it you can see a road. you have escaped
+-------------------------------------------------------------------------------
+"""
